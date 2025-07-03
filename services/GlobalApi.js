d8647897c8a0ba504425b2b9850d944b001a3568
meabhay <as3720719@gmail.com>
2025-07-04 04:12:51 +0530

    adding VITE_BASE_URL

--- services/GlobalApi.js
+++ services/GlobalApi.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "VITE_BASE_URL",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
